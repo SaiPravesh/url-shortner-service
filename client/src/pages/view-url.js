33101e669a27b60051b6262069d01e9e1501bfbb
SaiPravesh Vishnu <Saipravesh.vishnu@eir.ie>
2020-07-04 21:56:50 +0100

    Added basic error handling

--- client/src/pages/view-url.js
+++ client/src/pages/view-url.js
@@ -21,17 +21,22 @@ const useStyles = makeStyles({
 const ViewURL = () => {
     // State declaation and initialization
     const [urls, setUrls] = useState([]);
+    const [error, setError] = useState({});
     const classes = useStyles();
 
     // Function to fetch data
     const fetchData = async () => {
         let response = await fetch('http://localhost:5000/api/url/view-url')
             .then(res => res.json())
-            .then(res => setUrls(res));
+            .then(res => setUrls(res))
+            .catch(err => {
+                console.log(err.message);
+                setError(err);
+            });
         console.log(response);
     };
 
-    // Hook to handle side effect of component, in this case this is a replacement fro componentDidMount life-cycle
+    // Hook to handle side effect of component, in this case this is a replacement for componentDidMount life-cycle
     useEffect(() => {
         fetchData();
     }, []);
@@ -43,44 +48,58 @@ const ViewURL = () => {
                 {/* {urls.map(url => (
                     <div key={url._id}>{url.shortUrl}</div>
                 ))} */}
-                <TableContainer component={Paper}>
-                    <Table className={classes.table} aria-label='simple table'>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell align='center' style={{ paddingLeft: '1rem' }}>
-                                    ID
-                                </TableCell>
-                                <TableCell align='center'>Short URL</TableCell>
-                                <TableCell align='center'>Actual URL</TableCell>
-                                {/* <TableCell align='right'>URL code</TableCell> */}
-                                <TableCell align='center'>Date created</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {urls.map((url, index) => (
-                                <TableRow key={url._id}>
-                                    <TableCell
-                                        component='th'
-                                        scope='row'
-                                        align='center'
-                                        style={{ paddingLeft: '1rem' }}>
-                                        {index + 1}
+                {error === undefined && (
+                    <div
+                        style={{
+                            margin: `0 auto`,
+                            textAlign: `center`,
+                            color: `red`,
+                            fontWeight: `bold`,
+                            fontSize: `xx-large`,
+                        }}>
+                        Failed to fetch data from backend
+                    </div>
+                )}
+                {error !== undefined && (
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label='simple table'>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align='center' style={{ paddingLeft: '1rem' }}>
+                                        ID
                                     </TableCell>
-                                    <TableCell align='center'>
-                                        <a href={url.shortUrl}>{url.shortUrl}</a>
-                                    </TableCell>
-                                    <TableCell align='center' style={{ colWidth: '2em' }}>
-                                        <a href={url.longUrl}>{url.longUrl}</a>
-                                    </TableCell>
-                                    {/* <TableCell align='right'>{url.urlCode}</TableCell> */}
-                                    <TableCell align='center' style={{ width: '20%' }}>
-                                        <Moment format='YYYY-MM-DD HH:mm'>{url.date}</Moment>
-                                    </TableCell>
+                                    <TableCell align='center'>Short URL</TableCell>
+                                    <TableCell align='center'>Actual URL</TableCell>
+                                    {/* <TableCell align='right'>URL code</TableCell> */}
+                                    <TableCell align='center'>Date created</TableCell>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                            </TableHead>
+                            <TableBody>
+                                {urls.map((url, index) => (
+                                    <TableRow key={url._id}>
+                                        <TableCell
+                                            component='th'
+                                            scope='row'
+                                            align='center'
+                                            style={{ paddingLeft: '1rem' }}>
+                                            {index + 1}
+                                        </TableCell>
+                                        <TableCell align='center'>
+                                            <a href={url.shortUrl}>{url.shortUrl}</a>
+                                        </TableCell>
+                                        <TableCell align='center' style={{ colWidth: '2em' }}>
+                                            <a href={url.longUrl}>{url.longUrl}</a>
+                                        </TableCell>
+                                        {/* <TableCell align='right'>{url.urlCode}</TableCell> */}
+                                        <TableCell align='center' style={{ width: '20%' }}>
+                                            <Moment format='YYYY-MM-DD HH:mm'>{url.date}</Moment>
+                                        </TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                )}
             </section>
         </Layout>
     );
